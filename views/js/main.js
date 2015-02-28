82407d1de43e6d6c053f3834037e2b8e00603858
Jorge Zuniga <jz2767@att.com>
2015-02-28 10:36:04 -0600

    addEventListener for (var i = 200; i--;) {

--- views/js/main.js
+++ views/js/main.js
@@ -526,7 +526,8 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 15; i++) {
+  //for (var i = 0; i < 15; i++) {
+  for (var i = 200; i--;) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
