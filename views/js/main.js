16a46a222cfff7cd815af89a69e234ad7a8a9631
Jorge Zuniga <jz2767@att.com>
2015-02-28 12:29:07 -0600

    35 pizzas

--- views/js/main.js
+++ views/js/main.js
@@ -530,7 +530,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 25; i++) {
+  for (var i = 0; i < 35; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza2.png";
