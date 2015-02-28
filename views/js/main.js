1385314632c893e36f6117651ab49ccaf253103e
Jorge Zuniga <jz2767@att.com>
2015-02-28 12:54:04 -0600

    querySelectorAll to querySelector in dx and newwidth vars

--- views/js/main.js
+++ views/js/main.js
@@ -450,10 +450,10 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-	  var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-	  var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+	  var dx = determineDx(document.querySelector(".randomPizzaContainer"), size);
+	  var newwidth = (document.querySelector(".randomPizzaContainer").offsetWidth + dx) + 'px';
 	  var elements = document.querySelectorAll(".randomPizzaContainer");
-	 for (var i = 0; i < elements.length; i++) {       
+	 for (var i = 0; i < elements.length; i--) {       
       elements[i].style.width = newwidth;
     }
   }
