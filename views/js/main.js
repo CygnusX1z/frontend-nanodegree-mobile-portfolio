31cf21f323a9b834a54bcb3d896fd0713ebf94da
Jorge Zuniga <jz2767@att.com>
2015-02-28 12:57:48 -0600

    back to original changePizzaSizes(sizes)

--- views/js/main.js
+++ views/js/main.js
@@ -448,15 +448,16 @@ var resizePizzas = function(size) {
     return dx;
   }
 
+  
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-	  var dx = determineDx(document.querySelectorAll(".randomPizzaContainer"), size);
-	  var newwidth = (document.querySelectorAll(".randomPizzaContainer").offsetWidth + dx) + 'px';
-	  var elements = document.querySelectorAll(".randomPizzaContainer");
-	 for (var i = 0; i < elements.length; i--) {       
-      elements[i].style.width = newwidth;
+    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
+      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
     }
   }
+ 
   changePizzaSizes(size);
 
   // User Timing API is awesome
