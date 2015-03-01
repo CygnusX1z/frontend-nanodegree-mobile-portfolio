17cfea89ce861e4e1a10029508675c202aa75bfe
Jorge Zuniga <jz2767@att.com>
2015-03-01 13:50:23 -0600

    add var elements

--- views/js/main.js
+++ views/js/main.js
@@ -460,8 +460,9 @@ var resizePizzas = function(size) {
   function changePizzaSizes(size) {
 	  var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
 	  var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {       
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+	  var elements = document.querySelectorAll("randomPizzaContatiner");
+    for (var i = 0; i < elements.length; i++) {       
+      elements[i].style.width = newwidth;
     }
   }
   
